df13598db5c1299cdce386e6bed6e0db24ca92e5
Nastya Leniuski <nastyamaseik@gmail.com>
2019-12-27 01:19:17 +0200

    Компоненты

--- src/lab4/GraphicsDisplay.java
+++ src/lab4/GraphicsDisplay.java
@@ -327,6 +327,57 @@ public class GraphicsDisplay extends JPanel {
             canvas.drawString(label, (float)(point.getX() + 5.0D), (float)(point.getY() - bounds.getHeight()));
         }
     }
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        scaleX=this.getSize().getWidth() / (this.viewport[1][0] - this.viewport[0][0]);
+        scaleY=this.getSize().getHeight() / (this.viewport[0][1] - this.viewport[1][1]);
+        if ((this.graphicsData == null) || (this.graphicsData.size() == 0)) return;
+
+
+        Graphics2D canvas = (Graphics2D) g;
+        Stroke oldStroke = canvas.getStroke();
+        Color oldColor = canvas.getColor();
+        Font oldFont = canvas.getFont();
+        Paint oldPaint = canvas.getPaint();
+        /// поворот
+        if (clockRotate) {
+            AffineTransform at = AffineTransform.getRotateInstance(Math.PI/2, getSize().getWidth()/2, getSize().getHeight()/2);
+            at.concatenate(new AffineTransform(getSize().getHeight()/getSize().getWidth(), 0.0, 0.0, getSize().getWidth()/getSize().getHeight(),
+                    (getSize().getWidth()-getSize().getHeight())/2, (getSize().getHeight()-getSize().getWidth())/2));
+            canvas.setTransform(at);
+
+        }
+        if (antiClockRotate) {
+            AffineTransform at = AffineTransform.getRotateInstance(-Math.PI/2, getSize().getWidth()/2, getSize().getHeight()/2);
+            at.concatenate(new AffineTransform(getSize().getHeight()/getSize().getWidth(), 0.0, 0.0, getSize().getWidth()/getSize().getHeight(),
+                    (getSize().getWidth()-getSize().getHeight())/2, (getSize().getHeight()-getSize().getWidth())/2));
+            canvas.setTransform(at);
+
+
+        }
+        paintGrid(canvas);
+        if (showAxis)
+        {paintAxis(canvas);
+            paintLabels(canvas);
+        }
+        paintGraphics(canvas);
+        if (showMarkers) paintMarkers(canvas);
+
+        paintSelection(canvas);
+        canvas.setFont(oldFont);
+        canvas.setPaint(oldPaint);
+        canvas.setColor(oldColor);
+        canvas.setStroke(oldStroke);
+
+    }
+
+    private void paintSelection(Graphics2D canvas) {
+        if (!scaleMode) return;
+        canvas.setStroke(selectionStroke);
+        canvas.setColor(Color.BLACK);
+        canvas.draw(selectionRect);
+    }
 
 
 }
